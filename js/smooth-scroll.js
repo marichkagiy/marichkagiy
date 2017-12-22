e1c9a9a525f8106ac9996528550e8cbb4b98bd7b
marichkagiy <fedevych.m@gmail.com>
2017-12-22 09:45:27 +0000

    Inintial

--- js/smooth-scroll.js
+++ js/smooth-scroll.js
@@ -5,7 +5,7 @@ $(function() {
             event.preventDefault();
             $('html, body').stop().animate({
                 scrollTop: target.offset().top
-            }, 1000);
+            }, 2000);
         }
     });
 });
